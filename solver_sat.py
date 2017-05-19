dc91d436839c81f37ed0139a731e1524963dd707
dferri <dferri@users.noreply.github.com>
2017-05-19 15:08:15 +0200

    Disallow loops (done inside the system)

--- solver_sat.py
+++ solver_sat.py
@@ -16,9 +16,16 @@ def solve_sat(grid_start, size, debug=False):
     r, c = Ints("r c")
     rows, cols = size
     n = len(grid_start)
+    # This is the function that represents the grid
+    # grid[x, y] == k means the cell at (x, y) is of color k
     grid = Function("grid", IntSort(), IntSort(), IntSort())
+    # This function gives Z3 the knowledge of direction of the flows
+    # flow[x, y] == k means the cell at (x, y) is k-th cell of its flow
+    # e.g. flow[x, y] == 0 for every (x, y) starting point of a flow
+    flow = Function("flow", IntSort(), IntSort(), IntSort())
     s = Solver()
 
+    ### Grid constraints
     # Defined only inside the grid and values in the right range
     s.add(ForAll([r, c], Implies(And(r >= 0, r < rows,
                                      c >= 0, c < cols),
@@ -27,24 +34,11 @@ def solve_sat(grid_start, size, debug=False):
     s.add(ForAll([r, c], Implies(Or(r < 0, r >= size[0],
                                     c < 0, c >= size[1]),
                                  grid(r, c) == -1)))
+    # Set the initial conditions
     for i, ((x1, y1), (x2, y2)) in enumerate(grid_start):
         s.add(grid(x1, y1) == i)
         s.add(grid(x2, y2) == i)
     # Other constraints
-    # Top
-    # s.add(ForAll([r, c],
-    #              Implies(And(r > 0, c > 0, c < h - 1),
-    #                      Not(And(grid(r, c)
-    #                              == grid(r-1, c-1)
-    #                              == grid(r-1, c)
-    #                              == grid(r-1, c+1))))))
-    # DEEEEBUUUG
-    # solution = [[0, 1, 1, 2, 2], [0, 1, 3, 2, 4], [0, 1, 3, 2, 4], [0, 1, 3, 2, 4], [0, 0, 3, 4, 4]]
-    # for i, row in enumerate(solution):
-    #     for j, pup in enumerate(row):
-    #         s.add(grid(i, j) == pup)
-
-
     # Top
     s.assert_and_track(
         ForAll([r, c],
@@ -109,8 +103,6 @@ def solve_sat(grid_start, size, debug=False):
                                grid(r, c) == grid(r-1, c-1),
                                grid(r, c) == grid(r-1, c  ))))),
         "Top Left")
-
-
 
     start_points = [a for couple in grid_start for a in couple]
     # The number of adjacent (non diagonal) of the same color is exactly 2 for
@@ -191,20 +183,90 @@ def solve_sat(grid_start, size, debug=False):
                        + If(grid(i, j) == grid(i-1, j  ), 1, 0)),
                     "dir_2_8_K_{}_i_j_{}_{}".format(K, i, j))
 
+    ### Flow constraints
+    # Defined only inside the grid and >= 0 values
+    s.add(ForAll([r, c], Implies(And(r >= 0, r < rows,
+                                     c >= 0, c < cols),
+                                 flow(r, c) >= 0)))
+    s.add(ForAll([r, c], Implies(Or(r < 0, r >= rows,
+                                    c < 0, c >= cols),
+                                 flow(r, c) == -1)))
+
+    # Set the initial conditions
+    for i, ((x1, y1), (x2, y2)) in enumerate(grid_start):
+        s.add(flow(x1, y1) == 0)
+
+    # For every cell that is not a starting point there must be two adjacent
+    # cells with decreasing and increasing value respectively
+    dx1, dy1, dx2, dy2 = Ints("dx1, dy1, dx2, dy2")
+    for i in range(rows):
+        for j in range(cols):
+            if (i, j) in start_points:
+                s.assert_and_track(
+                    Exists(
+                        [dx1, dy1],
+                        And(# Adjacent cell
+                            Or((dx1 == 1), (dx1 == 0), (dx1 == -1)),
+                            Or((dy1 == 1), (dy1 == 0), (dy1 == -1)),
+
+                            Or(Not(dx1 == 0), Not(dy1 == 0)),
+                            # Inside the grid
+                            And((i + dx1) >= 0, (i + dx1) < rows,
+                                (j + dy1) >= 0, (j + dy1) < cols),
+                            # Of the same color
+                            grid(i, j) == grid(i + dx1, j + dy1),
+
+                            Or(flow(i, j) == (flow(i + dx1, j + dy1) + 1),
+                               flow(i, j) == (flow(i + dx1, j + dy1) - 1))
+                        )
+                    ), "flow_starting_i_j_{}_{}".format(i, j))
+            else:
+                s.assert_and_track(
+                    Exists(
+                        [dx1, dy1, dx2, dy2],
+                        And(# Different adjacent cells
+                            Or((dx1 == 1), (dx1 == 0), (dx1 == -1)),
+                            Or((dy1 == 1), (dy1 == 0), (dy1 == -1)),
+                            Or((dx2 == 1), (dx2 == 0), (dx2 == -1)),
+                            Or((dy2 == 1), (dy2 == 0), (dy2 == -1)),
+
+                            Or(Not(dx1 == dx2), Not(dy1 == dy2)),
+                            Or(Not(dx1 == 0), Not(dy1 == 0)),
+                            Or(Not(dx2 == 0), Not(dy2 == 0)),
+                            # Inside the grid
+                            And((i + dx1) >= 0, (i + dx1) < rows,
+                                (j + dy1) >= 0, (j + dy1) < cols),
+                            And((i + dx2) >= 0, (i + dx2) < rows,
+                                (j + dy2) >= 0, (j + dy2) < cols),
+                            # Of the same color
+                            grid(i + dx1, j + dy1) == grid(i, j),
+                            grid(i + dx2, j + dy2) == grid(i, j),
+
+                            flow(i, j) == (flow(i + dx1, j + dy1) + 1),
+                            flow(i + dx2, j + dy2) == (flow(i, j) + 1)
+                        )
+                    ), "flow_normal_i_j_{}_{}".format(i, j))
+
     if s.check() == unsat:
         if debug:
             print()
             print("UNSAT")
             for e in s.unsat_core():
                 print(e)
+            return None, None
         return None
 
     m = s.model()
-    return [[m.evaluate(grid(i, j)).as_long() for j in range(size[1])] for i in range(size[0])]
+    if debug:
+        return ([[m.evaluate(grid(i, j)).as_long() for j in range(size[1])]
+                 for i in range(size[0])],
+                [[m.evaluate(flow(i, j)).as_long() for j in range(size[1])]
+                 for i in range(size[0])])
+    else:
+        return [[m.evaluate(grid(i, j)).as_long() for j in range(size[1])]
+                for i in range(size[0])]
 
 def main():
-    # TODO: Check for Loops
-
     LEVEL_SIZE = (5, 5)
     INPUT = [[(0, 0), (4, 1)], [(0, 2), (3, 1)], [(0, 4), (3, 3)], [(1, 2), (4, 2)], [(1, 4), (4, 3)]]
     sol = solve_sat(INPUT, LEVEL_SIZE)
